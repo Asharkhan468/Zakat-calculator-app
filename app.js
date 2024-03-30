b492bdb3ef894787e214eb94ea97148fe9445491
Asharkhan468 <asharullah.khan2468@gmail.com>
2024-03-30 15:44:12 +0500

    Zakat calculator app

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 
-let cashField=document.getElementById("cash-field");
-let goldField=document.getElementById("gold-field");
-
+let cashField=document.querySelector("cash-field");
+let goldField=document.querySelector("gold-field");
+let paragraph=document.querySelector("#para");
 
 let goldPrice=19780;
 
+
 function calculateZakat(){
 
-    console.log(+goldField.value * goldPrice + +cashField.value);
+    let totalAmount=+goldField.value * goldPrice + +cashField.value;
+    paragraph.innerHTML+=`The Zakat `
     
     
 }
