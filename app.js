5e1741b2a162120da62c64a52735e3aa3521d40d
Asharkhan468 <asharullah.khan2468@gmail.com>
2024-03-30 21:56:30 +0500

    zakat app

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 
-let cashField=document.querySelector("cash-field");
-let goldField=document.querySelector("gold-field");
+let cashField=document.querySelector("#cash-field");
+let goldField=document.querySelector("#gold-field");
 let paragraph=document.querySelector("#para");
-
-let goldPrice=19780;
+let otherAssets=document.querySelector("#other-assets");
+let goldPrice=document.querySelector("#gold-price")
 
 
 function calculateZakat(){
 
-    let totalAmount=+goldField.value * goldPrice + +cashField.value;
-    paragraph.innerHTML+=`The Zakat `
+
+
+    let totalAmount=+goldField.value * +goldPrice.value + +cashField.value + +otherAssets.value;
+    let zakatAmount=2.5*totalAmount/100
+    paragraph.innerHTML=`Your total wealth is Rs ${totalAmount}/- So you have to pay 2.5% of Zakat which is Rs ${zakatAmount}/- `
     
     
 }
